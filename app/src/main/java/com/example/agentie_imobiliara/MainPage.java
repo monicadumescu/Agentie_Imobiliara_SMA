1a5f6f9001b297bb476689121a0555984ec1caa5
monicadumescu <dumescumonica@gmail.com>
2022-10-16 18:08:34 +0300

    Added menu base

--- app/src/main/java/com/example/agentie_imobiliara/MainPage.java
+++ app/src/main/java/com/example/agentie_imobiliara/MainPage.java
@@ -1,14 +1,107 @@
 package com.example.agentie_imobiliara;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import io.grpc.okhttp.internal.framed.Header;
+
 
 public class MainPage extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        FirebaseAuth authAction=FirebaseAuth.getInstance();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView user_text = (TextView) headerView.findViewById(R.id.user_text);
+        user_text.setText(authAction.getCurrentUser().getEmail().toString());
+        navigationView.getMenu();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.logutB:
+                    {
+                        authAction.signOut();
+                        break;
+                    }
+                    case R.id.houses:
+                    {
+                        break;
+                    }
+                    case R.id.liked_houses:
+                    {
+                        break;
+                    }
+                    case R.id.bookings:
+                    {
+                        break;
+                    }
+                    case R.id.boiking_history:
+                    {
+                        break;
+                    }
+                    case R.id.account:
+                    {
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
 }
